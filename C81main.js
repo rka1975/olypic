5039a19a67e45e6c6a3e987452e9b0e3d5e7e3db
rka1975 <rupeshkagrawal@gmail.com>
2021-07-24 21:09:15 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,49 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color="blue"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+color="black"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(300,200,40,0,2*Math.PI);
+ctx.stroke();
+color="red"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(400,200,40,0,2*Math.PI);
+ctx.stroke();
+color="yellow"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(250,250,40,0,2*Math.PI);
+ctx.stroke();
+color="green"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(350,250,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e)
+{
+    mouse_x=e.clientX-canvas.offsetLeft;
+mouse_y=e.clientY-canvas.offsetTop;
+console.log("X="+mouse_x+",Y="+mouse_y);
+circle(mouse_x,mouse_y);
+    
+}
+function circle(mouse_x,mouse_y){
+    color="blue"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+ctx.stroke();
+}
